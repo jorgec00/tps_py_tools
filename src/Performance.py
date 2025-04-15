43f40a2ed03f54edf5add0d5b998d9b92719a807
Jorge Cervantes <mars@Jorges-MacBook-Pro.local>
2025-04-15 16:04:45 -0700

    added level accel function to IADS data class

--- src/Performance.py
+++ src/Performance.py
@@ -48,65 +48,77 @@ class LJ_IADS_flight_data:
         self.Vic = self.Vic[~rm_mask]
         self.Hic = self.Hic[~rm_mask]
 
+    def process_level_accel(self, speeds: np.array, Tic: np.float64) -> pd.DataFrame:
+        """Process the IADS data to extract energy height and Mach number for level acceleration.
+        
+        Args:
+            - speeds: calibrated airpseed desired to extract from DAS data (try to match handwritten data)
+            - Tic: Recorded flight temperature (Kelvin)
+
+        Returns:
+            - Dataframe containing:
+                -- Time since beginning of maneuver
+                -- Energy height at each calibrated airspeed
+                -- Mach number at each calibrated airspeed
+                -- Specific excess power at each calibrated airspeed
+        
+        """
+        # Find index of flight_data set for desired speeds and average the nearest +/- 10 samples
+        df = pd.DataFrame()
+        for speed in speeds:
+            index = np.where(np.isclose(self.Vic, speed, atol=0.1))[0][0]
+            df = pd.concat([df, pd.DataFrame([{
+                'time': np.average(self.time[index-10:index+10]),
+                'Vic': np.average(self.Vic[index-10:index+10]) * 1.6878,  # to fps
+                'Hic': np.average(self.Hic[index-10:index+10]),
+                'Tic': Tic,
+            }])], ignore_index=True)
+
+        # Create a standard atmosphere object
+        atm = StandardAtmosphere()
+
+        # Calculate true airspeed
+        qcic_psl = calculate_calibrated_pressure_differential_ratio(df['Vic'])
+        qcic_ps = qcic_psl / atm.delta(df['Hic'])
+        ps = atm.delta(df['Hic']) * atm.constants.PRESSURE_SEA_LEVEL
+        qcic = qcic_ps * ps
+        Mic = calculate_mach(qcic, ps)
+        Vt = calculate_true_airspeed(Mic, df['Tic']/atm.constants.TEMP_SEA_LEVEL)
+
+        # Calculate energy height over time
+        energy_height = df['Hic'] + Vt**2 / 2
+
+        # Append Mic and energy height to dataframe
+        df['Mic'] = Mic
+        df['energy_height'] = energy_height
+
+        return df
+
+        
+
 def performance():
     """ Main Function """
-    """ Specifications """
-    # Denote sample rate and desired moving average time window
-    sample_rate = 100  # Hz
-    average_time = 0  # seconds
-    average_window = int((average_time+0.01)*sample_rate); # time converted to samples, 0.01 added for centered moving average
-
-    # Denote length of maneuver, in seconds
-    maneuver_time = 120 # seconds
-    maneuver_samples = int(maneuver_time * sample_rate)
-
     # Specify data file name
     filename = os.path.join('Performance', 'flight_data.csv')
-
-    # Manevuer (options - level accel: "LA", check climb: "C", check descent: "D", level sustained turn: "T")
-    maneuver = "LA"
 
     """Data import and pre-processing"""
     # Import flight data
     flight_data = LJ_IADS_flight_data(filename)
 
-    # Calculate the centered moving average 
-    flight_data.Vic = centered_moving_average(flight_data.Vic, average_window)
-    flight_data.Hic = centered_moving_average(flight_data.Hic, average_window)
-
+    # Process level acelleration data
+    speeds = np.array([257, 258, 259, 260, 261, 262, 263])
     # Recorded flight temp
-    Tic = np.full_like(flight_data.Hic, -10 + 273.15)  # Celsius to Kelvin
+    Tic = -10 + 273.15  # Celsius to Kelvin
+    # Process data
+    LA_data = flight_data.process_level_accel(speeds, Tic)
+    # Extract energy height and Mach number
+    energy_height = LA_data['energy_height']
+    Mic = LA_data['Mic']
+    # Extract time
+    time = LA_data['time']
 
-    # Select only the maneuver data
-    flight_data.Hic = flight_data.Hic[:maneuver_samples]
-    flight_data.Vic = flight_data.Vic[:maneuver_samples]
-    flight_data.time = flight_data.time[:maneuver_samples]
-    Tic = Tic[:maneuver_samples]
-
-    # Downsample for plotting
-    flight_data.Hic = flight_data.Hic[::average_window]
-    flight_data.Vic = flight_data.Vic[::average_window]
-    flight_data.time = flight_data.time[::average_window]
-    flight_data.Tic = Tic[::average_window]
-    
-    # Create a standard atmosphere object
-    atm = StandardAtmosphere()
-
-    """Process Level Accelleration Data"""
-    if maneuver == "LA":
-        # Calculate true airspeed
-        qcic_psl = calculate_calibrated_pressure_differential_ratio(flight_data.Vic)
-        qcic_ps = qcic_psl / atm.delta(flight_data.Hic)
-        ps = atm.delta(flight_data.Hic) * atm.constants.PRESSURE_SEA_LEVEL
-        qcic = qcic_ps * ps
-        Mic = calculate_mach(qcic, ps)
-        Vt = calculate_true_airspeed(Mic, flight_data.Tic/atm.constants.TEMP_SEA_LEVEL)
-
-        # Calculate energy height over time
-        energy_height = flight_data.Hic + Vt**2 / 2
-
-        # Plot
-        plot_energy_height_mach(flight_data.time, energy_height, Mic)
+    # Plot
+    plot_energy_height_mach(time, energy_height, Mic)
 
 if __name__ == "__main__":
     performance()
